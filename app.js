73ea423ef73a0dadf3826803ac7317e7a05a1ebc
Fredrick F Khoury <ffkhoury@crimson.ua.edu>
2014-12-06 07:58:05 -0600

    first plzz

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+//config
+app.set('views', __dirname + '');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname + '/'));
+
+
+//routes
+app.get('/',function(req,res){
+	res.render('index.html')
+});
+
+//server
+app.listen(1337,function(){
+	console.log('ready on port 1337');
+})
+
